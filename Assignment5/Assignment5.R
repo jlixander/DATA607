12ab0641c6507e5813ed2bf52274e15125754a30
jlixander <88115818+jlixander@users.noreply.github.com>
2022-10-16 23:26:10 -0400

    Completed assignment5

--- Assignment5/Assignment5.R
+++ Assignment5/Assignment5.R
@@ -1,5 +1,4 @@
 library(tidyverse)
-library(rjson)
 library(jsonlite)
 library(RJSONIO)
 library(RCurl)
@@ -10,19 +9,19 @@ library(rvest)
 
 #Load json file-----------------------------------------------------------------
 url_json = "https://raw.githubusercontent.com/jlixander/DATA607/main/Assignment5/Assignment5.json"
-json_df = fromJSON((url_json)) %>% 
-  as.data.frame
+json_df = jsonlite::fromJSON((url_json), simplifyDataFrame = TRUE) %>% 
+  as.data.frame()
 
 
 #Load xml file------------------------------------------------------------------
 url_xml = GET("https://raw.githubusercontent.com/jlixander/DATA607/main/Assignment5/assignment5.xml") #retreive xml file
 xml_doc <- xmlParse(url_xml) #parse xml file
-xml_df <- xmlToDataFrame(doc, nodes=getNodeSet(doc, "//TopBooks")) #Select which level to unlist.
+xml_df <- xmlToDataFrame(xml_doc, nodes=getNodeSet(xml_doc, "//TopBooks")) #Select which level to unlist.
 
 
 #Load html file-----------------------------------------------------------------
 html_url <- "https://raw.githubusercontent.com/jlixander/DATA607/main/Assignment5/Assignment5.html"
-
 html_doc <- xml2::read_html(html_url)
-html_doc <- xmlParse(html_doc) #parse xml file
-html_df <- xmlToDataFrame(html_doc, nodes=getNodeSet(html_doc, "//TopBooks")) #Select which level to unlist.
+tables = html_doc |> html_table()
+table_one = tables[[1]]
+html_df <- head(table_one, - 1)  
